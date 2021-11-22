7cc5e9debccf7ee6c71d709ab4474363bfc2c2e7
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-11-22 17:48:11 -0500

    Initial commit with a few working methods and tests

---
+++ src/test/java/OurHashMapTest.java
@@ -0,0 +1,134 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OurHashMapTest {
+
+    @Test
+    void getNull() {
+        // given
+        OurHashMap<String,String> map = new OurHashMap<>();
+
+        // when
+
+        // then
+        assertNull(map.get("ENGLISH1"));
+    }
+
+    @Test
+    void put() {
+        // given
+        OurHashMap<String,String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("ENGLISH2", "HI");
+        map.put("ENGLISH3", "HEY");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+        map.put("RUSSIAN", "PRIVIT");
+        map.put("JAPANESE", "NE HOW");
+        map.put("MANDARIN", "CONICHIWA");
+
+        // then
+        assertEquals("HELLO", map.get("ENGLISH1"));
+        assertEquals("HI", map.get("ENGLISH2"));
+        assertEquals("HEY", map.get("ENGLISH3"));
+        assertEquals("HOLA", map.get("SPANISH"));
+        assertEquals("SHALOM", map.get("HEBREW"));
+        assertEquals("BONJOUR", map.get("FRENCH"));
+        assertEquals("PRIVIT", map.get("RUSSIAN"));
+        assertEquals("NE HOW", map.get("JAPANESE"));
+        assertEquals("CONICHIWA", map.get("MANDARIN"));
+    }
+
+    @Test
+    void putSameKeys() {
+        // given
+        OurHashMap<String,String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+        map.put("ENGLISH1", "HI");
+
+        // then
+        assertEquals("HI", map.get("ENGLISH1"));
+    }
+
+    @Test
+    void size() {
+        // given
+        OurHashMap<String,String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("ENGLISH2", "HI");
+        map.put("ENGLISH3", "HEY");
+
+        // then
+        assertEquals(3, map.size());
+    }
+
+    @Test
+    void isEmpty () {
+        // given
+        OurHashMap<String,String> map = new OurHashMap<>();
+
+        // when
+
+        // then
+        assertEquals(true, map.isEmpty());
+    }
+
+    @Test
+    void clear() {
+        // given
+        OurHashMap<String,String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("ENGLISH2", "HI");
+        map.put("ENGLISH3", "HEY");
+        map.clear();
+
+        // then
+        assertEquals(0, map.size());
+    }
+
+    @Test
+    void containsKey() {
+        // given
+        OurHashMap<String,String> map = new OurHashMap<>();
+
+        // when
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+        map.put("RUSSIAN", "PRIVIT");
+        map.put("JAPANESE", "NE HOW");
+        map.put("MANDARIN", "CONICHIWA");
+
+        // then
+        assertTrue(map.containsKey("RUSSIAN"));
+        assertFalse(map.containsKey("ENGLISH"));
+    }
+//
+//    @Test
+//    void containsValue() {
+//        // given
+//        OurHashMap<String,String> map = new OurHashMap<>();
+//
+//        // when
+//        map.put("HEBREW", "SHALOM");
+//        map.put("FRENCH", "BONJOUR");
+//        map.put("RUSSIAN", "PRIVIT");
+//        map.put("JAPANESE", "NE HOW");
+//        map.put("MANDARIN", "CONICHIWA");
+//
+//        // then
+//
+//    }
+}
